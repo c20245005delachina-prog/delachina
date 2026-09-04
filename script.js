2fc8a9584a1f4f5f70c0fdcf12c1583601aa75d4
c20245005delachina-prog <c20245005.delachina@csav.edu.ph>
2026-09-04 14:39:15 +0800

    variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log("Hello, World!");
 console.log("From script.js file.");
+
+const myName = "Cuennie";
+let age = 19;
+age = 20;
+myNumber = 9462707644;
+myaddress = "Manapla";
+
+console.log(`My name is ${myName}, ${age} years old.`);
+console.log(`Number: ${myNumber}`);
+console.log(`Address: ${myaddress}`);
+
+console.log(67);
+
