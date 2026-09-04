a75279d0e3d3c74eb9b5cec234be6de0dc3dffc9
c20245005delachina-prog <c20245005.delachina@csav.edu.ph>
2026-09-04 16:52:10 +0800

    textContent

--- script.js
+++ script.js
@@ -23,10 +23,14 @@ function greet(greeting, names = []) {
 console.log(greet("Mayung aga" , ["Cuennie", " Bai", " Soy", " Tol", " Erp"]));
 
 const heading = document.querySelector("h1");
-console.log(heading);
+heading.textContent = "Ernie";
+console.log(heading.textContent);
 
 const contactHeading = document.querySelector("#contact h2");
 console.log(contactHeading);
 
 const projectsHeading = document.querySelector("#services h2");
 console.log(projectsHeading);
+
+
+
